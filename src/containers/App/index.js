a2f8da6634d74c22779dedf3510325d12daab54b
d.vakatsiienko <d.vakatsiienko@lectrum.io>
2017-08-24 10:35:07 +0300

    update setup script

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { getCurrentTime } from '../../helpers';
 import Styles from './styles.scss';
 
 export default class App extends Component {
@@ -6,6 +7,9 @@ export default class App extends Component {
         return (
             <section className = { Styles.app }>
                 <h1>Welcome!</h1>
+                <p>
+                    It is {getCurrentTime()}
+                </p>
             </section>
         );
     }
---
+++ src/helpers/index.js
@@ -0,0 +1,3 @@
+import moment from 'moment';
+
+export const getCurrentTime = () => moment().format('MMMM D h:mm a');
