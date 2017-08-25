eb49d0da4b33d73cda6d021a47b022dbcb62fdea
d.vakatsiienko <d.vakatsiienko@lectrum.io>
2017-08-25 19:56:42 +0300

    add hash function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,6 +3,22 @@ import moment from 'moment';
 
 export const getCurrentTime = () => moment().format('MMMM D h:mm:ss a');
 
+export const generateHash = (length, lowercase) => {
+    let text = '';
+    let possible =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    if (lowercase) {
+        possible = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    }
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+};
+
 export function getRandomColor () {
     const letters = '0123456789ABCDEF';
     let color = '#';
