366f9b6598075a1778a540830a1b04b56bda5ce8
d.vakatsiienko <d.vakatsiienko@lectrum.io>
2017-08-25 14:17:41 +0300

    add random color function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,3 +2,14 @@
 import moment from 'moment';
 
 export const getCurrentTime = () => moment().format('MMMM D h:mm:ss a');
+
+export function getRandomColor () {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
