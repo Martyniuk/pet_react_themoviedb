b62f54f507771b93479e1958c35b702a42ae02a9
Vladimir Martyniuk <vl.martyniuk@gmail.com>
2017-11-03 18:47:01 +0200

    test for WishListItem file created

---
+++ src/components/WishListItem/WishListItem.test.js
@@ -0,0 +1,11 @@
+// Core
+import React from 'react';
+import WishListItem from './';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from 'enzyme';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('WishListItem component', () => {
+    
+});
