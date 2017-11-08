55c057110d6ea972a0595efb93b2bacaa7d365d8
Vladimir Martyniuk <vl.martyniuk@gmail.com>
2017-11-08 08:53:49 +0200

    footer added and styled

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,18 +3,31 @@ import React, { Component } from 'react';
 
 // instruments
 import Styles from './styles.scss';
+import PropTypes from 'prop-types';
 
 // components
 import SearchBar from '../SearchBar';
 import Header from '../Header';
+import Content from "../Content/index";
 
 export default class Main extends Component {
     render () {
         return (
             <div className = { Styles.main }>
-                <Header />
-                <div>
-                    <SearchBar />
+                <div className = { Styles.wrapper }>
+                    <Header />
+                    <div>
+                        <SearchBar />
+                    </div>
+                    <div className = { Styles.content }>
+                        {/*<Content
+                            latestMoviesList = {}
+                            mostPopularMoviesList = {}
+                        />*/}
+                    </div>
+                    <div className = { Styles.footer }>
+                        <p>Designed By _へ__(‾◡◝ )&#62; </p>
+                    </div>
                 </div>
             </div>
         );
